d8518817b64c1c5a47d9a1fa8d03b9973448e28d
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-03-28 03:39:36 +0900

    [Modify] SimpleTableViewExampleViewController - UIViewController 상속으로 변경

--- RxSwiftPractice/Examples/SimpleTableViewExampleViewController.swift
+++ RxSwiftPractice/Examples/SimpleTableViewExampleViewController.swift
@@ -6,16 +6,19 @@
 //
 
 import UIKit
+import SnapKit
 import RxSwift
 import RxCocoa
 
-final class SimpleTableViewExampleViewController: UITableViewController {
+final class SimpleTableViewExampleViewController: ViewController, UITableViewDelegate{
+    let tableView = UITableView()
     let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureView()
+        tableView.delegate = self
         
         let items = Observable.just(
             (0..<20).map { "\($0)" }
@@ -46,6 +49,10 @@ final class SimpleTableViewExampleViewController: UITableViewController {
 
     private func configureView() {
         view.backgroundColor = .white
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 }
